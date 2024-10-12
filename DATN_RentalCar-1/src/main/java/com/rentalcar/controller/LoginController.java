2544a2549ebb29a10a4c276ca3d1f67a7e4d2f4a
yuuki <sondinhm1014@gmail.com>
2024-10-13 02:40:47 +0700

    Login with session update
    chức năng đăng nhập với session

--- DATN_RentalCar-1/src/main/java/com/rentalcar/controller/LoginController.java
+++ DATN_RentalCar-1/src/main/java/com/rentalcar/controller/LoginController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.rentalcar.entity.Account;
 import com.rentalcar.service.AcountService;
+import com.rentalcar.service.SessionService;
 
 @Controller
 @RequestMapping(value = "/login")
@@ -17,6 +18,8 @@ public class LoginController {
 	
     @Autowired
     private AcountService acountService;
+    @Autowired
+    private SessionService session;
 
     // Phương thức GET để hiển thị trang đăng nhập
     @GetMapping
@@ -30,6 +33,11 @@ public class LoginController {
         Account account = acountService.findByEmail(email);
 
         if (account != null && account.getPasswordHash().equals(password)) {
+        	session.set("user", account);
+        	
+        	Account loggedAcc = session.get("user");
+    		System.out.println("User from session: " +loggedAcc.getUsername());
+
             return "redirect:/home";  // Chuyển hướng đến trang home sau khi đăng nhập thành công
         } else {
             model.addAttribute("error", "Email hoặc mật khẩu không hợp lệ");
