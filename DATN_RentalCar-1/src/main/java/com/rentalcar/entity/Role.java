9b2c654169dc1de99368eb88954c5f090f2f58f5
anct001 <vietantran1230@gmail.com>
2024-10-16 23:07:25 +0700

    sửa2

--- DATN_RentalCar-1/src/main/java/com/rentalcar/entity/Role.java
+++ DATN_RentalCar-1/src/main/java/com/rentalcar/entity/Role.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.*;
 @Entity
