921c0f80ccb0811ff66b59be6f7f7d917a57b354
Dương Hữu Khánh <khanhconngoan@gmail.com>
2024-12-16 02:11:55 +0700

    update FeebackRepo

--- DATN_RentalCar-1/src/main/java/com/rentalcar/service/impl/FeedbackServiceImpl.java
+++ DATN_RentalCar-1/src/main/java/com/rentalcar/service/impl/FeedbackServiceImpl.java
@@ -14,26 +14,50 @@ import com.rentalcar.service.FeedbackService;
 
 @Service
 public class FeedbackServiceImpl implements FeedbackService{
-	@Autowired
-    private FeedbackRepo feedbackRepository;
+	@Autowired FeedbackRepo fbRepo;
+	
+	@Override
+	public List<FeedbackInfo> getCompletedRentalsWithFeedbackMotorbike(Long motorbikeId) {
+	    return fbRepo.getCompletedRentalsWithFeedbackMotorbike(motorbikeId);  // Truyền motorbikeId vào
+	}
 
-    @Override
-    public List<FeedbackInfo> getCompletedRentalsWithFeedbackMotorbike(Long motorbikeId) {
-        // Giả sử bạn có một phương thức trong FeedbackRepo để lấy thông tin này
-        return feedbackRepository.findCompletedRentalsWithFeedbackMotorbike(motorbikeId);
-    }
+	@Override
+	public List<FeedbackInfo> getCompletedRentalsWithFeedbackCar(Long carId) {
+	    return fbRepo.getCompletedRentalsWithFeedbackCar(carId);  // Truyền carId vào repository method
+	}
 
-    @Override
-    public List<FeedbackInfo> getCompletedRentalsWithFeedbackCar(Long carId) {
-        // Giả sử bạn có một phương thức trong FeedbackRepo để lấy thông tin này
-        return feedbackRepository.findCompletedRentalsWithFeedbackCar(carId);
-    }
 
-    @Override
-    public List<FeedbackInfo> getFeedbackByCustomer(Long customerId) {
-        // Lấy thông tin phản hồi của khách hàng từ repository
-        return feedbackRepository.findFeedbackByCustomer(customerId);
-    }
+	public <S extends Feedback> S save(S entity) {
+		return fbRepo.save(entity);
+	}
+
+	public List<Feedback> findAll() {
+		return fbRepo.findAll();
+	}
+
+	public Optional<Feedback> findById(Long id) {
+		return fbRepo.findById(id);
+	}
+
+	public boolean existsById(Long id) {
+		return fbRepo.existsById(id);
+	}
+
+	public <S extends Feedback> boolean exists(Example<S> example) {
+		return fbRepo.exists(example);
+	}
+
+	public long count() {
+		return fbRepo.count();
+	}
+
+	public void deleteById(Long id) {
+		fbRepo.deleteById(id);
+	}
+
+	public Feedback getById(Long id) {
+		return fbRepo.getById(id);
+	}
 	
 	
 }
